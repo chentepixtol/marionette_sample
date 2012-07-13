69eabcfd7e52ca2f3d67f57be36d943da1430a28
vmendoza <vmendoza@promologistics.com.mx>
2012-07-13 10:14:52 -0500

    varios cambios

--- js/app.js
+++ js/app.js
@@ -1,8 +1,16 @@
-define(["marionette"], function(Marionette){
+define(["marionette", "vendor/twig"], function(Marionette, twig){
 
   var app = new Marionette.Application();
 
   app.addInitializer(function(options){
+
+    console.log(twig);
+
+    Marionette.TemplateCache.get = function(templateId){
+      return twig({
+        data: $(templateId).text()
+      });
+    };
     
     app.addRegions({
       menuRegion: "#menu",
